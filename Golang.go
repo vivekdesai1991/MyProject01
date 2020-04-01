d577d95ff0f429d053d99321063f60aa51d05fa5
vinaydesai13 <62844160+vinaydesai13@users.noreply.github.com>
2020-04-01 18:16:23 +0530

    Update Golang.go

--- Golang.go
+++ Golang.go
@@ -2,13 +2,4 @@ package main
 import "fmt"
 func main() {
    fmt.Printf("Hello World")
-   fmt.Printf("Branch feature")
-   fmt.Printf("1st commit")
-   fmt.Printf("2nd commit")
-   fmt.Printf("3rd commit")
-   fmt.printf("new branch")
-   fmt.Printf("4th commit")
-   fmt.Printf("new branch commit 1")
-   fmt.Printf("new branch commit 2")
-   fmt.Printf("new branch commit 3")
 }
